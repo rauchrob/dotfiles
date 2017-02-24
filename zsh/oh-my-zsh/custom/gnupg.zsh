390b52995c3742f05b3007e35e7a74728da1cbeb
Robert Rauch <mail@robertrauch.de>
2017-02-24 18:16:26 +0100

    zsh: fix gpg-agent support

--- zsh/oh-my-zsh/custom/gnupg.zsh
+++ zsh/oh-my-zsh/custom/gnupg.zsh
@@ -1,4 +1,6 @@
-# load gpg agent info, if available
-if [ -r ~/.gpg-agent-info ]; then
-    source ~/.gpg-agent-info
+# use gpg-agent ssh auth sock, if available
+GPG_AGENT_SOCK=~/.gnupg/S.gpg-agent.ssh
+
+if [ -r $GPG_AGENT_SOCK ]; then
+  export SSH_AUTH_SOCK=$GPG_AGENT_SOCK
 fi
